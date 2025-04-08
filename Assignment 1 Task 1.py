2c600872f2b4f8971c8098f623f49e88ac595e9a
Prashant Bhadauria <prashantbhadauria@Prashants-MacBook-Pro.local>
2025-04-09 00:35:45 +0530

    Add Assignment 1 Task 1 and Assignment 2 Task 2 scripts with updated README

---
+++ Assignment 1 Task 1.py
@@ -0,0 +1,12 @@
+a=input("Enter the first number: ")
+a=float(a)
+b=input("Enter the second number: ")
+b=float(b)
+c=a + b
+d=a - b
+e=a * b
+f=a /b
+print("\nAddition: " ,c)
+print("Subtraction: " ,d)
+print("Multiplication: " ,e)
+print("Division: " ,f)
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Python-Codes
+
+Assignment 1 Task 1 : For basic maths calculations
+
+Assignment 1 Task 2 : To display a personalised message
+
